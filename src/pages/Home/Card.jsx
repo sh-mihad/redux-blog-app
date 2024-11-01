313fc16b8c10222acb139145f4a1a7ec16fa20da
Sabbir <sh.mihad.cse@gmail.com>
2024-11-01 23:20:40 +0600

    videos ui done

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -1,25 +1,40 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 
+import { Link } from "react-router-dom";
 
+export default function Card({ blog }) {
+  const { id, title, description, image, tags, likes, isSaved, createdAt } =
+    blog || {};
 
-export default function Card() {
   return (
     <div className="lws-card">
-    <a href="post.html">
-      <img src="./images/git.webp" className="lws-card-image" alt="" />
-    </a>
-    <div className="p-4">
-      <div className="lws-card-header">
-        <p className="lws-publishedDate">2023-05-01</p>
-        <p className="lws-likeCount"><i className="fa-regular fa-thumbs-up"></i>100</p>
-      </div>
-      <a href="post.html" className="lws-postTitle"> Top Github Alternatives </a>
-      <div className="lws-tags"><span>#python,</span> <span>#tech,</span> <span>#git</span></div>
-    
-      <div className="flex gap-2 mt-4">
+      <Link to={`blog/${id}`}>
+        <img src={image} className="lws-card-image" alt="" />
+      </Link>
+      <div className="p-4">
+        <div className="lws-card-header">
+          <p className="lws-publishedDate">{createdAt}</p>
+          <p className="lws-likeCount">
+            <i className="fa-regular fa-thumbs-up"></i>
+            {likes}
+          </p>
+        </div>
+        <Link  to={`blog/${id}`} className="lws-postTitle">
+          {title}
+        </Link>
+        <div className="lws-tags">
+          {tags?.map((item) =>
+            tags.length - 1 ? <span key={item}>#{item}</span> : <span key={item}>#{item}</span>
+          )}
+        </div>
+
+      {
+        isSaved &&   <div className="flex flex-wrap gap-2 mt-4 ">
         <span className="lws-badge"> Saved </span>
       </div>
-    
+      }
+      </div>
     </div>
-  </div>
-  )
+  );
 }
